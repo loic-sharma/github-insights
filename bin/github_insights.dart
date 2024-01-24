a5038365438c8ff216cb82b212f03159d63bd2ec
Loic Sharma <sharma.loic@gmail.com>
2024-01-24 09:01:54 -0800

    Initial commit

---
+++ bin/github_insights.dart
@@ -0,0 +1,5 @@
+import 'package:github_insights/github_insights.dart' as github_insights;
+
+void main(List<String> arguments) {
+  print('Hello world: ${github_insights.calculate()}!');
+}
---
+++ test/github_insights_test.dart
@@ -0,0 +1,8 @@
+import 'package:github_insights/github_insights.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
